5a34da343ef35cf3c28e0676e43d6cdcb71bbbaf
alireza <alirezaabdolmaleki2278@gmail.com>
2023-02-25 19:12:13 +0330

    change file test function.py

--- test Function.py
+++ test Function.py
@@ -41,3 +41,65 @@
 #     print("max:", max(x ,y, z))
 # Max()
 #----------------------------------------------------------------
+# پیاده سازی کار داخلی len توسط len2
+# def len2(x):
+#     i = 0
+#     for _ in x:
+#         i+=1
+#     return i
+# a = input("enter: ")
+# print(len2(a))
+#-----------------------------------------------------------------
+#پیاده سازی کار تابع داخلی max یا min
+# def max2(*args):
+#     m = args[0]
+#     for i in args:
+#         if i > m:
+#             i = m
+#     return m
+#
+# print(max2(1,99,70,100,36))
+#--------------------------------------------------------------------
+# #پیاده سازی تابع داخلی sum
+# def sum2(it):
+#     x=0
+#     for i in it:
+#         x+=i
+#     return x
+#
+# print(sum2([1, 99, 70, 100, 36]))
+#---------------------------------------------------------------------
+#تابع تشخیص مربع بودن یا نبودن یک عدد
+# def aquare():
+#     z=int(input("Enter number: "))
+#     for i in range(1,z):
+#         if i**2 == z:
+#             print(f"yes :) {i} * {i} = {z}")
+#             break
+#     else:
+#             print("no!")
+# aquare()
+#------------------------------------------------------------------
+#تابع محاسبه قیمت کالا همراه تخفیف
+# def price(price,discount_percent):
+#     discount_rate = int(price * discount_percent / 100)
+#     new_price = price - discount_rate
+#     print("price",price)
+#     print("Discounted price",new_price)
+#
+#
+# price1 = float(input("Enter price: "))
+# discount_percent = float(input("Enter discount percent: "))
+# price(price1,discount_percent)
+#-------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
